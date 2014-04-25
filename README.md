45e855c9c7b0f134434bf52256e7ace777ee6c0b
singno <158590301@qq.com>
2014-04-25 14:41:08 +0800

    String extend for plus.js

---
+++ README.md
@@ -0,0 +1,312 @@
+Plus.string
+===
+
+String manipulation extensions for Plus.js javascript library.
+
+#### _.string.capitalize(str)
+
+字符串首字母大写。
+
+	_.string.capitalize('foo');
+	=> 'Foo'
+	
+#### _.string.titleize(str)
+
+字符串所有单词大写。
+
+	_.string.titleize('this is my book');
+	=> 'This Is My Book'
+	
+#### _.string.format(str, data)
+
+简单的字符串模板填充。
+
+	_.string.format('my name is {name}, i am {age} year old.', {
+		name: 'jerry',
+		age: 12
+	});
+	=> 'my name is jerry, i am 12 year old.'
+	
+#### _.string.trim(str) *ecmascript 5*
+
+首尾空白字符过滤。
+
+	_.string.trim(' foo ');
+	=> 'foo'
+	
+#### _.string.trimLeft(str) *ecmascript 5*
+
+首部空白字符过滤。
+
+	_.string.trimLeft(' foo ');
+	=> 'foo '
+
+#### _.string.trimRight(str) *ecmascript 5*
+
+尾部空白字符过滤。
+	
+	_.string.trimLeft(' foo ');
+	=> ' foo'
+	
+#### _.string.clean(str)
+
+过滤掉首尾空白字符，并将字符串中（非首尾）连续的多个空白字符替换为1个。
+
+	_.string.clean('  this   is   my    book ');
+	=> 'this is my book'
+
+#### _.string.swapCase(str)
+
+大小写字母互换。
+
+	_.string.swapCase('hELLO');
+	=> 'Hello'
+	
+#### _.string.escapeHTML(str)
+
+html转义。
+
+	_.string.escapeHTML('<>&\'"');
+	=> '&lt;&gt;&amp;&#x27;&quot;'
+	
+#### _.string.unescapeHTML(str)
+
+将转义后的html还原。
+
+	_.string.unescapeHTML('&lt;&gt;&amp;&#x27;&quot;&#162;&#xa2;');
+	=> '<>&\'"¢¢'
+	
+#### _.string.toHTMLEntity(str)
+
+将字符串转换为html实体字符。
+
+	_.string.toHTMLEntity('>&');
+	=> '&#62;&#38;'
+	
+#### _.string.fromHTMLEntity(str)
+
+将html实体转换为常规字符串。
+
+	_.string.fromHTMLEntity('&#62;&#38;'); 
+	=> '>&'
+	
+	
+#### _.string.toUnicode(str)
+
+将字符串转换为unicode编码。
+
+	_.string.toUnicode('bar');
+	=> '\\u0062\\u0061\\u0072'
+
+#### _.string.fromUnicode(str)
+
+将unicode编码的字符串转换为常规字符串。
+
+	_.string.fromUnicode('\\u0062\\u0061\\u0072');
+	=> 'bar' 
+
+#### _.string.reverse(str)
+
+字符串反转。
+
+	_.string.reverse('foobar');
+	=> 'raboof'
+	
+#### _.string.splice(str, index, howmany, substr)
+
+_.string.splice 方法向/从字符串中添加/删除字符串，然后返回新的字符串。
+
+	_.string.splice('foobar', 3, 2, 'ab');
+	=> 'fooabr'
+	
+#### _.string.insert(str, index, substr)
+
+在字符串中`index`的位置插入子字符串。
+
+	_.string.insert('tab', 1, 'c');
+	=> 'tcab'
+	
+#### _.string.camelize(str)
+
+将字符串转换为"驼峰式"。
+
+	_.string.camelize('margin-top');
+	=> 'marginTop'
+	
+#### _.string.classify(str)
+
+将字符串转换为"类名式"。
+
+	_.string.classify('some_class_name');
+	=> 'SomeClassName'
+	
+#### _.string.underscored(str)
+
+将字符串转换为"下划线式"。
+
+	_.string.underscored('margin-top');
+	_.string.underscored('marginTop');
+	=> 'margin_top'
+
+#### _.string.dasherize(str)
+
+将字符串转换为"连字符式"。
+
+	_.string.dasherize('marginTop');
+	=> 'margin-top'
+	
+#### _.string.humanize(str)
+
+将字符串转换为人类可读的。
+
+	_.string.humanize('  capitalize dash-CamelCase_underscore trim  ');
+	=> 'Capitalize dash camel case underscore trim'
+	
+#### _.string.truncate(str, len, truncateStr)
+
+将超出len长度的字符串截断。
+
+	_.string.truncate('hello world', 5);
+	=> 'hello...'
+	
+#### _.string.repeat(str, count, separator)
+
+重复一个字符串count次。
+
+	_.string.repeat('a', 5);
+	=> 'aaaaa'
+	
+#### _.string.surround(str, wrap)
+
+将字符串用另一字符串`wrap`包裹。
+
+	_.string.surround('me', '$');
+	=> $me$
+
+#### _.string.pad(str, len, [padStr], [type])
+ 
+用padStr（默认空格）填充字符串的首尾两边。
+
+	_.pad('foo', 3); // padStr 默认为空格，type默认为'left'
+	=> '   foo' 
+	_.pad('foo', 3, '$', 'right');
+	=> 'foo$$$'
+	_.pad('foo', 3, '$', 'both');
+	=> '$$$foo$$$'
+	
+#### _.string.lpad(str, len, padStr)
+
+同`_.pad(str, len, padStr, 'left');`
+
+#### _.string.rpad(str, len, padStr)
+
+同`_.pad(str, len, padStr, 'right');`
+
+#### _.string.lrpad(str, len, padStr)
+
+同`_.pad(str, len, padStr, 'both');`
+
+#### _.string.strRight(str, pattern, [offset])
+
+在str中从左至右搜寻`pattern`字符串，并返回字符串在`pattern`右边的部分。
+	
+	_.string.strRight('foobarfoobar', 'foo');
+	=> 'barfoobar'
+
+#### _.string.strRightBack(str, pattern, [offset])
+	
+在str中从右至左搜寻`pattern`字符串，并返回字符串在`pattern`右边的部分。
+
+	_.string.strRightBack('f-g-h', '-', 2);	
+	=> 'g-h'
+
+#### _.string.strLeft(str, pattern, [offset])
+
+在str中从左至右搜寻`pattern`字符串，并返回字符串在`pattern`左边的部分。
+ 	
+ 	_.string.strLeft('foobarfoobar', 'bar');
+ 	=> 'foo'
+ 	
+#### _.string.strLeftBack(str, pattern, [offset])
+
+在str中从右至左搜寻`pattern`字符串，并返回字符串在`pattern`左边的部分。
+
+	_.string.strLeftBack('f-f-f', '-', 1);
+	=> 'f'
+	
+#### _.string.stripTags(str)
+
+过滤掉string中的html标签。
+
+	_.string.stripTags('<abc>321</abc><input value="a" /><span>123</span>');
+	=> '321123'
+
+#### _.string.escapeRegExp(str)
+
+转义正则表达式。
+
+	var str = '.*+?^=!:${}()|[]/\\',
+		regexp = new RegExp(_.string.escapeRegExp(str));
+		
+	regexp.test(str);
+	=> true
+	
+#### _.string.count(str, substr)
+
+计算子字符串在字符串中出现的次数。
+
+	_.string.count('foofoo', 'oo');
+	=> 2
+
+#### _.string.isBlank(str);
+
+判断字符串是否空白字符。
+
+	_.string.isBlank('\n');
+	=> true
+	
+#### _.string.startsWith(str, substr)
+
+判断字符串是否以子字符串开头。
+
+	_.string.startsWith('hello world', 'hello');
+	=> true
+	
+_.string.endsWith(str, substr)
+
+判断字符串是否以子字符串结尾。
+
+	_.string.endsWith('hello world', 'world');
+	=> true
+
+_.string.chop(str, step)
+
+将字符串按每组长度为step分为n段。
+
+	_.string.chop('abcefghi', 3);
+	=> ['abc', 'efg', 'hi']
+	
+_.string.isUpper(str)
+
+判断字符是否是大写字母。
+
+	_.string.isUpper('ABC');
+	=> true
+	
+_.string.isLower(str)
+
+判断字符是否是小写字母。
+
+	_.string.isLower('abc');
+	=> true
+	
+
+	
+
+
+
+	
+	
+
+
+	
---
+++ plus.string.js
@@ -0,0 +1,484 @@
+/*
+ * Plus.string.js 1.0.1
+ * https://github.com/singno/plus/
+ *
+ * Copyright 2014, singno
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ *
+ */
+;(function (root) {	// `root == window` in the browser, `root == exports` on the server.  
+	'use strict';
+
+	// Native APIS of `ECMAScript 5`.
+	var nativeTrim = String.prototype.trim,
+		nativeTrimLeft = String.prototype.trimLeft,
+		nativeTrimRight = String.prototype.trimRight;
+
+	// Short Reference of core prototypes.
+	var slice = Array.prototype.slice;
+
+	// Cache RegExps instead of constructing them everytime. 
+	var rtitleize = /(^|\s)([a-z])/gi,
+		rformat = /\{([^}]+)\}/g,
+		rtrimLeft = /^\s+/,
+		rtrimRight = /\s+$/,
+		rwhiteSpace = /\s+/g,
+		rletters = /[a-zA-Z]/g,
+		rescapeEntity = /[<>&"']/g,
+		runescapeEntity = /&([^;]+);/g,
+		rentityCodeHex = /^#x([\da-fA-F]+)$/,
+		rentityCode = /^#(\d+)$/,
+		runderscored = /[\s\-]+/g,
+		rblank = /^\s*$/,
+		rcamelize = /(?:_+)(\w)/g,
+		rlowerUpper = /([a-z])([A-Z])/g,
+		rdasherize = /[\-_\s]+/g,
+		runderscore = /_+/g,
+		rupper = /[A-Z]/g,
+		rtags = /<[^>]*?>/g,
+
+		// Escape `.*+?^=!:${}()|[]/\` characters.
+		rescapeRegExp = /([.*+?^=!:${}()|[\]\/\\])/g,
+		runicode = /\\u([a-f0-9]{4})/gi,
+		rHTMLEntity = /&#([^;]+);/g,
+
+		// All chars are upperCase
+		rstrUpper = /^[A-Z]+$/g,
+
+		// All chars are lowerCase
+		rstrLower = /^[a-z]+$/g;
+
+	var escapeEntity = {
+	    "<": "&lt;",
+	    ">": "&gt;",
+	    "&": "&amp;",
+	    "\"": "&quot;",
+	    "'": "&#x27;"
+	},
+		unescapeEntity = {};
+
+	for (var prop in escapeEntity) {
+		if (escapeEntity.hasOwnProperty(prop)) {
+			unescapeEntity[escapeEntity[prop]] = prop;
+		}
+	}
+
+	function extend (target) {
+		var source;
+
+		for (var i = 1, len = arguments.length; i < len; i++) {
+			source = arguments[i];
+			if (typeof source !== 'object') {
+				continue;
+			}
+
+			for (var prop in source) {
+				if (source.hasOwnProperty(prop)) {
+					target[prop] = source[prop];
+				}
+			}
+		}
+
+		return target;
+	}
+
+	function S (str) {
+		if (!(this instanceof S)) {
+			return new S(str);
+		}
+
+		str += '';
+		this._string = str;
+		this._chain = false;
+	}
+
+	extend(S, {
+		VERSION: '1.0.1',
+
+		count: function (str, substr) {
+			str += '';
+			return str.split(substr).length - 1;
+		},
+
+		isBlank: function (str) {
+			return rblank.test(str);
+		},
+
+		startsWith: function (str, substr) {
+			return str.indexOf(substr) === 0;
+		},
+
+		endsWith: function (str, substr) {
+			str += '';
+			substr += '';
+
+			var idx = str.lastIndexOf(substr);
+			return str.length === idx + substr.length;
+		},
+
+		chop: function (str, step) {
+			str += '';
+			step = Math.floor(Math.abs(step)) || 1;
+			var parts = [];
+			for (var i = 0, len = str.length; i < len;) {
+				parts.push(str.slice(i, i += step));
+			}
+
+			return parts;
+		},
+
+		isUpper: function (str) {
+			return rstrUpper.test(str);
+		},
+
+		isLower: function (str) {
+			return rstrLower.test(str);
+		}
+	}, {
+		capitalize: function (str) {
+			str += '';
+	     	return str.charAt(0).toUpperCase() + str.slice(1); 
+	    },
+
+	    titleize: function (str) {
+			str += '';
+	       	return str.replace(rtitleize, function (s, blank, letter) {
+	       		return blank + letter.toUpperCase();
+	       	});
+		},
+
+	    format: function (str, data) {
+		    str += '';
+		    data = data || {};
+
+		    var formatStr = str.replace(rformat, function(match, paren){
+		        if(data.hasOwnProperty(paren)){
+		            return data[paren];
+		        }
+
+		        return match;
+		    });
+		    return formatStr;
+		},
+
+		trim: function (str) {
+			str += '';
+			return nativeTrim ? nativeTrim.call(str) : str.replace(rtrimLeft, '').replace(rtrimRight, ''); 	
+		},
+
+		trimLeft: function (str) {
+			str += '';
+			return nativeTrimLeft ? nativeTrimLeft.call(str) : str.replace(rtrimLeft, '');
+		},
+
+		trimRight: function (str) {
+			str += '';
+			return nativeTrimRight ? nativeTrimRight.call(str) : str.replace(rtrimRight, '');
+		},
+
+		clean: function (str) {
+			str = S.trim(str);
+			return str.replace(rwhiteSpace, ' ');
+		},
+
+		swapCase: function (str) {
+			str += '';
+			return str.replace(rletters, function (c) {
+				return c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
+			});
+		},
+
+		escapeHTML: function (str) {
+			str += '';
+			return str.replace(rescapeEntity, function (entity) {
+		        return escapeEntity[entity];
+		    });
+		},
+
+		unescapeHTML: function (str) {
+			str += '';
+			str = str.replace(runescapeEntity, function (entity, entityCode) {
+				var match;
+
+				if (unescapeEntity.hasOwnProperty(entity)) {
+					return unescapeEntity[entity];
+				}
+
+				if (match = entityCode.match(rentityCodeHex)) {
+					return String.fromCharCode(parseInt(match[1], 16));
+				}
+
+				if (match = entityCode.match(rentityCode)) {
+					return String.fromCharCode(match[1]);
+				}
+
+				return entity;
+			});
+			return str;
+		},
+
+		toHTMLEntity: function (str, radix) {
+			str += '';
+			radix = +radix || 10;
+			var result = [];
+
+			for (var i = 0, len = str.length; i < len; i++) {
+				// `&#x{hexCharCode};` for hexadecimal; `&#{decimalCharCode};` for decimal.
+				//  For example: ['&#x3e;', '&#62;'] both is '>';
+				result.push('&#' + (radix === 16 ? 'x' : '') + str.charCodeAt(i).toString(radix) + ';');
+			}
+
+			return result.join('');
+		},
+
+		fromHTMLEntity: function (str) {
+			return str.replace(rHTMLEntity, function (match, code) {
+				// If the code is hexadecimal
+				if (code.charAt(0) === 'x') {
+					code = '0' + code;
+				}
+
+				return String.fromCharCode(code);
+			});
+		},
+
+		toUnicode: function (str) {
+			str += '';
+		    var result = [],
+		        unicode;
+
+		    for (var i = 0, len = str.length; i < len; i++) {
+		    	// The unicode format is `\u{hexCharCode}`
+		    	// If the `String(hexCharCode).length < 4`, left pad with '0'.
+		        unicode = str.charCodeAt(i).toString(16);
+		        unicode = '\\u0000'.slice(0, -unicode.length) + unicode;
+		        result.push(unicode);
+		    }
+
+		    return result.join(''); 
+		},
+
+		fromUnicode: function (str) {
+			str += '';
+
+			return str.replace(runicode, function (m, hex) {
+				return String.fromCharCode(parseInt(hex, 16));
+			});
+		},
+
+		reverse: function (str) {
+			str += '';
+			return str.split('').reverse().join('');
+		},
+
+		splice: function (str, idx, count, substr) {
+			str += '';
+			idx -= 0;
+			count -= 0;
+			var parts = [
+				str.slice(0, idx),
+				str.slice(idx + count + (idx > 0 ? 0 : str.length))
+			]; 
+			return parts.join(substr);
+		},
+
+		insert: function (str, idx, substr) {
+			return S.splice(str, idx, 0, substr);
+		},
+
+		underscored: function (str) {
+			return S.trim(str).replace(runderscored, '_').replace(rlowerUpper, '$1_$2').toLowerCase();
+		},
+
+		camelize: function (str) {
+			return S.underscored(str).replace(rcamelize, function (m, letter) {
+				return letter.toUpperCase();
+			});
+		},
+
+		classify: function (str) {
+			str = S.camelize(str);
+			return str.charAt(0).toUpperCase() + str.slice(1);
+		},
+
+		dasherize: function (str) {
+			return S.trim(str).
+					replace(rupper, '-$&').
+					replace(rdasherize, '-').
+					toLowerCase();	
+		},
+
+		humanize: function (str) {
+			return S(str).chain().underscored().capitalize().value().replace(runderscore, ' ');
+		},
+
+		truncate: function (str, len, truncateStr) {
+			str += '';
+			len = len || 0;
+			truncateStr = truncateStr == null ? '...' : truncateStr;
+
+			if (str.length <= len) {
+				return str;
+			}
+
+			str = str.slice(0, len);
+			return str + truncateStr;
+		},
+
+		sprintf: function () {
+			// todo	
+		},
+
+		repeat: function (str, count, separator) {
+			var arr = [];
+			separator = separator == null ? '' : separator;
+
+			for (var i = 0; i < count; i++) {
+				arr.push(str);
+			}
+			return arr.join(separator);
+		},
+
+		surround: function (str, wrap) {
+			str += '';
+			wrap = wrap == null ? '' : String(wrap); 
+			return wrap + str + wrap;
+		},
+
+		pad: function (str, len, padStr, type) {
+			padStr = padStr == null ? ' ' : padStr;
+			type = type || 'left';
+			var repeatStr = S.repeat(padStr, len);
+
+			switch (type) {
+				case 'right':
+					return str + repeatStr;
+				case 'both':
+					return repeatStr + str + repeatStr;
+				case 'left':
+				case 'default':
+					return repeatStr + str;
+			}	
+		},
+
+		lpad: function (str, len, padStr) {
+			return S.pad(str, len, padStr, 'left');
+		},
+
+		rpad: function (str, len, padStr) {
+			return S.pad(str, len, padStr, 'right');
+		},
+
+		lrpad: function (str, len, padStr) {
+			return S.pad(str, len, padStr, 'both');
+		},
+
+		strRight: function (str, pattern, offset) {
+			str += '';
+			pattern += '';
+
+			var idx = str.indexOf(pattern, offset);
+
+			if (idx === -1) {
+				return str;
+			}
+
+			return str.slice(idx + pattern.length);
+		},
+
+		strRightBack: function (str, pattern, offset) {
+			str += '';
+			pattern += '';
+
+			var idx = str.lastIndexOf(pattern, offset);
+
+			if (idx === -1) {
+				return str;
+			}
+
+			return str.slice(idx + pattern.length);
+		},
+
+		strLeft: function (str, pattern, offset) {
+			str += '';
+
+			var idx = str.indexOf(pattern, offset);
+
+			if (idx === -1) {
+				return str;
+			}
+
+			return str.slice(0, idx);
+		},
+
+		strLeftBack: function (str, pattern, offset) {
+			str += '';
+
+			var idx = str.lastIndexOf(pattern, offset);
+
+			if (idx === -1) {
+				return str;
+			}
+
+			return str.slice(0, idx);
+		},
+
+		stripTags: function (str) {
+			str += ''
+			return str.replace(rtags, '');
+		},
+
+		escapeRegExp: function (str) {
+			str += '';
+			return str.replace(rescapeRegExp, '\\$1');
+		}
+	});
+
+	var fn = S.prototype;
+
+	fn.value 
+	= fn.valueOf
+	= fn.toString
+	= fn.toLocaleString
+	= function () {
+		return this._string;
+	};
+
+	fn.chain = function () {
+		this._chain = true;
+		return this;
+	};
+
+	for (var prop in S) {
+		if (typeof S[prop] !== 'function') {
+			continue;
+		}
+
+		(function (prop) {
+			fn[prop] = function () {
+				var argv = [this._string].concat(slice.call(arguments)),
+					str = S[prop].apply(this, argv); 
+
+				return this._chain ? S(str).chain() : str;	
+			};
+		})(prop); 
+	}
+
+	if (typeof (module) !== 'undefined' && module.exports) { // CommonJS
+        module.exports = S;
+    } else if (typeof (define) === 'function' && define.amd) { // AMD
+        define('plus.string', function () {
+            return S;
+        });
+    } else { // Exports to global namespace.
+    	var _ = root._ = root._ || {};
+        _.str = _.string = S;
+    }
+
+})(this);
+
+
+
+
